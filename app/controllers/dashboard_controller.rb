dbf38e338dda280152a60dd018629c10fb26f444
Pavel Goloblia <iverick091@gmail.com>
2023-03-23 23:34:37 +0200

    Functionality to add and modify avatars for user's profile

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,24 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_user!, :render_navbar
+  before_action :authenticate_user!, expect: [:show]
+  before_action :set_user, only: [:show]
+  before_action :render_navbar
 
   def index
+  end
+
+  def appearance
+  end
+
+  def show
+    redirect_to dashboard_path if @user.nil?
   end
 
   private
     def render_navbar
       @render_navbar = true
+    end
+
+    def set_user
+      @user = User.find_by_id(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'dashboard', to: "dashboard#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
+  get 'dashboard', to: 'dashboard#index'
+  get 'appearance', to: 'dashboard#appearance'
   root 'dashboard#index'
+
+  get ':id', to: 'dashboard#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
